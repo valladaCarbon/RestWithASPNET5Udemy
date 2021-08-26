8eb491cf163a3e2b9108ab1c7028486ee299bd73
Anderson Viana <valladau2@gmail.com>
2021-08-26 09:52:28 -0300

    Criando a Classe service

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+    }
+}
