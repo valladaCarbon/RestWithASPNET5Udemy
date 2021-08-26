939a020a2c55f9a69ca54be7abe2cfc192026cf9
Anderson Viana <valladau2@gmail.com>
2021-08-26 19:49:51 -0300

    Criando a Service PersonService como injeção de dependência

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using RestWithASPNETUdemy.Services.Implementations;
 
 namespace RestWithASPNETUdemy
 {
@@ -21,6 +22,9 @@ namespace RestWithASPNETUdemy
         {
 
             services.AddControllers();
+
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithASPNETUdemy", Version = "v1" });
